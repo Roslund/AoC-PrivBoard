d07a4ae72050c7ab43b8cf8c080e50053465314c
Roslund <enari@minit.nu>
2020-12-11 23:30:01 +0100

    Added namemapping

--- index.js
+++ index.js
@@ -44,11 +44,24 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
+const namemapping = (id) => {
+  switch (id) {
+    case '382854':  return 'Anton Roslund'
+    case '402019':  return 'Erik Östlund'
+    case '1167042': return 'Fadi Bitar'
+    case '1196812': return 'Stefan Hermansen'
+    case '1186141': return 'Tobias Fendin'
+    case '1159859': return 'Jonas Tano'
+    case '1159859': return 'Jimmy Wimmersjö'
+    case '1111749': return 'Jakob Huss'
+    default: return null
+  }
+}
 
 app.get('/', (req, res) => {
   if (new Date().getTime() - lastUpdated >= 600000) { updateLeaderboard() }
   console.log('Updating leaderboard')
-  res.render('index', { leaderboard: leaderboard })
+  res.render('index', { leaderboard: leaderboard, namemapping: namemapping })
 })
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
